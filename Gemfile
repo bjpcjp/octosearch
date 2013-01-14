85b207672191c0bb9bb007e9927b7a395f7ea2ce
brian piercy <bjpcjp@gmail.com>
2013-01-14 15:22:37 -0600

    added postgresql to gem file, production only

--- Gemfile
+++ Gemfile
@@ -24,6 +24,12 @@ end
 gem 'jquery-rails'
 gem 'octopart'
 
+
+group :production do
+	gem 'pg', '0.12.2'
+end
+
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,8 @@ OctopartTest::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
+root :to => 'apis#index'
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
